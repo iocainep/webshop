c834a0b11dea6ca90152cc25d299bfa6e9480794
iocaine <lonermine@gmail.com>
2021-03-04 10:03:25 +0300

    Доделал главную страницу и страницу регистрации со всеми эффектами

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+var button = document.getElementById('menu_btn');
+var hover = document.getElementById('hover');
+var menu = document.getElementById('nav');
+var close = document.getElementById('close-menu');
+
+button.addEventListener('click', function () {
+    menu.classList.toggle('display-block');
+    hover.classList.toggle('display-block');
+});
+
+close.addEventListener('click', function () {
+    menu.classList.toggle('display-block');
+    hover.classList.toggle('display-block');
+});
+
